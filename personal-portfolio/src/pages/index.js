4e8febda6a67de6e8f16391ec97296a91a71e082
Dayton Kotas <Kotasdm@gmatc.matc.edu>
2024-06-28 16:20:24 -0500

    Fixed homepage styling and layout

--- personal-portfolio/src/pages/index.js
+++ personal-portfolio/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import styled from "styled-components"
+import * as styles from "./styles/home.module.css" // Use named import
 
 const HomeContainer = styled.div`
   display: flex;
@@ -10,15 +11,21 @@ const HomeContainer = styled.div`
   height: 100vh;
   text-align: center;
 `
-const Home = () => (
-  <Layout>
-    <HomeContainer>
-      <h1>Welcome to My Personal Portfolio</h1>
-      <p>
-        Hi, I'm Dayton Kotas, a software developer specializing in web development.
-      </p>
-    </HomeContainer>
-  </Layout>
-)
 
-export default Home
+const HomePage = () => {
+  return (
+    <Layout>
+      <HomeContainer>
+        <div className={styles.content}>
+          <h1>Welcome to My Personal Portfolio</h1>
+          <p>
+            Hi, I'm Dayton Kotas, a software developer specializing in web
+            development. Explore my projects and learn more about me.
+          </p>
+        </div>
+      </HomeContainer>
+    </Layout>
+  )
+}
+
+export default HomePage
